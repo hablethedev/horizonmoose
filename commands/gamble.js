40bdb507e9ca6a1c4671c60fa3faf2c58e78a838
Hazle <xendyex@tutanota.com>
2024-12-30 20:05:35 +0000

    debug
    debug

--- commands/gamble.js
+++ commands/gamble.js
@@ -10,11 +10,12 @@ module.exports = {
 		const member = interaction.guild.members.cache.get(userId);
 
 		// sorry sea
-		if (userId === '949691680796328046') {
+		if (userId === '949691680796328046' || userId === '1113284520737771621') {
 			const isInBotSpam = interaction.channel.id == botSpamChannelId;
 
 			if (!isInBotSpam) {
 				await interaction.reply('Go to bot-spam in legitidevs, you have been banned from gambling anywhere else. <#1319649023346479144>');
+				console.log(interaction.channel.id)
 				return; 
 			}
 		}
